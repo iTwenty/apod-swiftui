48c5b8580c03bb9f13dbd641212b37d7ac04734a
Jaydeep Joshi <jaydeepmjoshi@gmail.com>
2022-02-06 12:44:41 +0530

    Initial Commit

---
+++ apod-swiftui/ApodApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApodApppp.swift
+//  apod-swiftui
+//
+//  Created by Jaydeep Joshi on 06/02/22.
+//
+
+import SwiftUI
+
+@main
+struct ApodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ apod-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  apod-swiftui
+//
+//  Created by Jaydeep Joshi on 06/02/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
